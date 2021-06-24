32702c8102075e84f2c6e70f72fd5c653d8b5e2c
Usuario <fmarvega@gmail.com>
2021-06-24 13:31:19 +0200

    Zeeman splitting added

---
+++ Functions/zeeman_splitting.m
@@ -0,0 +1,39 @@
+% Función que calcula la convolución con la temperatura de una densidad de
+% estados a la que se le aplica un desdoblamiento zeeman.
+%-------------------------------------------------------------------------
+% INPUTS:   Voltage - Vector con los valores de voltaje
+%           DOS - Vector con los valores de la densidad de estados
+%           VZeeman - Desplazamiento zeeman en mV
+%           Temperature - Temperatura
+%           NormSup - Voltage de normalización superior
+%           NormInf - Voltage de normalización inferior
+%-------------------------------------------------------------------------
+
+function [Conductance] = zeeman_splitting(Voltage,DOS,VZeeman,Temperature,NormSup,NormInf)
+
+NPoints = length(Voltage);
+
+VoltageStep = abs(Voltage(1)-Voltage(2));
+Pixel_Zeeman = round(VZeeman/VoltageStep); %Calculo el número de píxeles a desplazar
+
+DOSPlus = [ones(NPoints+Pixel_Zeeman,1);DOS;ones(NPoints-Pixel_Zeeman,1)];
+DOSMinus = [ones(NPoints-Pixel_Zeeman,1);DOS;ones(NPoints+Pixel_Zeeman,1)];
+
+DOSAUX = DOSPlus+DOSMinus;
+DOSAUX = DOSAUX(NPoints+1:2*NPoints);
+% fig=figure;
+% plot(Voltage,DOSAUX)
+
+%Función de Fermi
+kB = 8.617e-2; %meV/K
+Beta = 1/(kB*Temperature);
+dFermiDist = (Beta*exp(Beta*Voltage))./((1+exp(Voltage*Beta)).^2);
+
+%Convolucionamos
+Conductance = conv(dFermiDist,DOSAUX,'same');
+% fig=figure;
+% plot(Voltage,Conductance)
+%Normalizamos
+Conductance = normalizacionPA(NormSup,NormInf,Voltage,Conductance,2048,2048);
+
+end
