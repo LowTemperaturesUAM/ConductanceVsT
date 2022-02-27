a89df307895fe329893f38215b3b2995a00f7568
Miguel Agueda <53227622+miguelagve@users.noreply.github.com>
2022-02-27 23:59:55 +0100

    Directories are now propertly adressed on Linux and MacOS platforms
    Auto mode is now working correctly

--- Functions/selectPath.m
+++ Functions/selectPath.m
@@ -1,18 +1,18 @@
 function [Voltage,MatrizCorriente,Temperature,MagneticField,FilePath] = selectPath(ForthBack,startPath)
 FilePath = uigetdir(startPath);
-A = load([FilePath '/Temperature.mat']);
-B = load([FilePath '/MagneticField.mat']);
+A = load([FilePath filesep 'Temperature.mat']);
+B = load([FilePath filesep 'MagneticField.mat']);
 Temperature = A.Temperature;
 MagneticField = B.MagneticField;
 
-NCurv = length(dir([FilePath, '/*.blq']));
-curvas = ReducedblqreaderV6([FilePath, '/1.blq']);
+NCurv = length(dir([FilePath, filesep, '*.blq']));
+curvas = ReducedblqreaderV6([FilePath, filesep, '1.blq']);
 % Voltage = curvas(ForthBack).data(1);
 Voltage = zeros(length(curvas(ForthBack).data),NCurv);
 MatrizCorriente = zeros(length(curvas(ForthBack).data),NCurv);
 
 for i=1:NCurv
-    FileName = ['/' int2str(i) '.blq'];
+    FileName = [filesep int2str(i) '.blq'];
     curvas = ReducedblqreaderV6([FilePath, FileName]);
     Voltage(:,i) = curvas(ForthBack).data(:,1);
     MatrizCorriente(:,i) = curvas(ForthBack).data(:,2);
