039d0c6614372801235d16a7e5d84f8f87252f18
Pablo García <110162220+pablogarciat@users.noreply.github.com>
2023-08-28 13:13:44 +0200

    Fixing errors when importing or calculating Fermi with arrays

--- Functions/FermiDeriv.m
+++ Functions/FermiDeriv.m
@@ -1,5 +1,5 @@
 function dFermiDist = FermiDeriv(Temperature,E)
     kB = 8.617e-2;
-    Beta = 1/(kB*Temperature);
-    dFermiDist = Beta./(4*(cosh(Beta*E/2)).^2);
+    Beta = 1./(kB*Temperature);
+    dFermiDist = Beta./(4*(cosh(Beta.*E/2)).^2);
 end
--- Functions/importVariable.m
+++ Functions/importVariable.m
@@ -1,5 +1,14 @@
 function [varOut] = importVariable(text) %varIn es un string
 [FileName, FilePath] = uigetfile('*.mat',text);
+
+% Prevent error if no file is selected/ action is cancelled
+if isequal(FileName,0)
+    varOut=0;
+    return
+end
+
+% Load struct with variables in file
 varOut = load([FilePath FileName]);
+% There should be only a variable named Struct. We take it as our output.
 varOut = varOut.Struct;
 end
